646cf1ae66b5d784773973809a381961e40df768
HuaGeWuDi <hualintianxia@foxmail.com>
2018-03-15 10:14:44 +0800

    StudyProgram

---
+++ StudyProgram/StudyProgram/Common/InfoHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace StudyProgram.Common
+{
+    public class InfoHelper
+    {
+        private static string LoginInfo = "LoginInfo";
+
+        public static void SetLoginInfo(LoginModel loginInfo)
+        {
+            SSHelper.SetSession(LoginInfo, loginInfo);         
+        }
+
+        public static LoginModel GetLoginInfo()
+        {
+            object info = SSHelper.GetSession(LoginInfo);
+            LoginModel loginModel = info as LoginModel;
+            return loginModel;
+        }
+    }
+
+    public class LoginModel
+    {
+        public string LoginNum { get; set; }
+        public string LoginName { get; set; }
+    }
+}
---
+++ StudyProgram/StudyProgram/Common/SSHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.SessionState;
+
+namespace StudyProgram.Common
+{
+    public class SSHelper
+    {
+        private static HttpSessionState _session = HttpContext.Current.Session;
+
+        //新增或修改
+        public static void SetSession(string key, object value)
+        {
+            _session[key] = value;
+        }
+
+        //删除指定key
+        public static void RemoveSession(string key)
+        {
+            _session.Remove(key);
+        }
+
+        //获取指定key的Session
+        public static object GetSession(string key)
+        {
+            return _session[key];
+        }
+
+        //清空Session
+        public static void Clear()
+        {
+            _session.Clear();
+        }
+    }
+}
